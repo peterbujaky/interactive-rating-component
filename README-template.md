73b9a35d53eddf569a41f5549c0f21841c1a6379
peterbujaky <104648835+peterbujaky@users.noreply.github.com>
2022-11-14 19:41:46 +0100

    Add files via upload

---
+++ README-template.md
@@ -0,0 +1,54 @@
+# Frontend Mentor - Interactive rating component solution
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+
+### Screenshot
+
+![](./design/Finished_screenshot.JPG) ![](./design/Finished_screenshot2.JPG)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Javascsript
+
+
+### What I learned
+
+This challenge was  good way to put my JS skills to a a test. I also installed and tried out SCSS.
+
+### Continued development
+
+Improve on writing a cleaner CSS code, simplify and avoid repetitions.
+
---
+++ script.js
@@ -0,0 +1,39 @@
+const submittButton = document.getElementById("submitButton");
+const ratingButtons = document.getElementsByClassName("btn-rating");
+const thankYouDiv = document.getElementById("thankyoudiv");
+const thankYouImgDiv = document.getElementById("thankyouimg");
+const thankyouText = document.createElement("p");
+const thankYouImg = document.createElement("img");
+thankYouImg.src = "///C:/Users/DELL/Documents/Frontendmentor%20Challenges/interactive-rating-component-main/images/illustration-thank-you.svg"
+
+
+            for (var i = 0; i < ratingButtons.length; i++ ) {
+            ratingButtons[i].addEventListener("click", function() {
+                var rating = this.innerHTML;
+
+                submittButton.addEventListener("click", function() {
+                
+                    document.getElementById('component').style.textAlign = "center";
+                    document.querySelector('h3').innerHTML = "Thank You!";;
+                    document.querySelector('p').innerHTML = "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!";
+
+                    thankYouImgDiv.appendChild(thankYouImg);
+                    thankYouDiv.appendChild(thankyouText);
+                    thankyouText.innerHTML = `You selected  ${rating} out of 5`;
+                    thankyouText.classList.add("ratingText");
+                    thankYouImgDiv.classList.add("thankyouimg");
+                    
+                    
+
+                    document.querySelector(".buttons").remove();
+                    document.getElementById("iconstar").remove();
+                    submittButton.remove();
+
+                })
+            })
+        }
+
+
+
+
+      
